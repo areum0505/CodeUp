ec2f5c8b6d6cc9feb9e7f3a162324f06753d873a
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-08 07:17:37 +0900

    1581 : (함수 작성+포인터) swap 함수 만들기 (Call by Reference)

---
+++ 2020 01/20200108.c
@@ -0,0 +1,18 @@
+﻿/* 첫 번째 포인터가 가리키는 변수의 값이 두 번째 포인터가 가리키는 변수의 값보다 클 경우 두 값을 서로 바꾼다. */
+
+#include <stdio.h>
+
+void myswap(int* a, int* b) {
+	if (*a > *b) {
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+int main(void) {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	myswap(&a, &b);
+	printf("%d %d", a, b);
+}

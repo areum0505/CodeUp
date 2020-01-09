844e578ebb055ab613a91d48b30caf4e1068fb0b
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-10 07:25:17 +0900

    1578 : (함수 작성) 최댓값 함수

---
+++ 2020 01/20200110.c
@@ -0,0 +1,16 @@
+﻿/* mymax 함수를 구현하여 두 정수 중 큰 수를 출력한다. */
+
+#include <stdio.h>
+
+int mymax(int a, int b) {
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", mymax(a, b));
+}

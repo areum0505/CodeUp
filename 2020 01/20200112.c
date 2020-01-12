195fa8418eb5e8d963c33bd55ec02e3abb8e029f
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-12 21:10:07 +0900

    1630 : 기상송 바꾸기

---
+++ 2020 01/20200112.c
@@ -0,0 +1,17 @@
+﻿/* 기상곡과 기상곡 사이에 AMOLED가 삽입되게 한다. */
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	char song[100][100];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%s", &song[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%s \n", song[i]);
+		if (i != n - 1)
+			printf("AMOLED \n");
+	}
+}

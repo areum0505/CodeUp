4f4321e73a2fb1c2e388c26c4a2bb0b79878c82d
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-19 14:42:36 +0900

    1290 : 대금 만들기

---
+++ 2020 01/20200119.c
@@ -0,0 +1,14 @@
+﻿/* 대나무의 원래의 길이를 n이라고 할 때, 이 대나무를 어떤 길이로 잘랐을 때 이 길이가 n의 약수이면 이 대나무는 좋은 소리를 내는 악기가 될 수 있다. */
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	int count = 0;
+	scanf("%d", &n);
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0)
+			count++;
+	}
+	printf("%d \n", count);
+}

9c87f92f68ad00a03f5c3e27eee049a38250008d
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-20 20:53:25 +0900

    1351 : 구구단 출력하기 2

---
+++ 2020 01/20200120.c
@@ -0,0 +1,13 @@
+﻿/* 시작단과 마지막 단을 입력하면 그 구간의 구구단을 출력하는 프로그램을 작성하시오. */
+
+#include <stdio.h>
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for (int i = n; i <= m; i++) {
+		for (int j = 1; j <= 9; j++) {
+			printf("%d*%d=%d\n", i, j, i*j);
+		}
+	}
+}

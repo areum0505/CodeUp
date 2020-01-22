5c284f792fd6555a4e782203e8a3b631e844499f
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-22 20:44:44 +0900

    1360 : 숫자 피라미드 2

---
+++ 2020 01/20200122.c
@@ -0,0 +1,22 @@
+﻿/*	5 5 5 5 5
+
+	4 4 4 4
+
+	3 3 3
+
+	2 2
+
+	1  */
+
+#include <stdio.h>
+
+int main() {
+	int num;
+
+	scanf("%d", &num);
+	for (int i = num; i > 0; i--) {
+		for (int j = 0; j < i; j++)
+			printf("%d ", i);
+		printf("\n");
+	}
+}

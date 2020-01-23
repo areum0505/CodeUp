dff8952e2968a57d4a55f4f21a28efb059734400
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-23 22:56:53 +0900

    Add files via upload

---
+++ 2020 01/20200123.c
@@ -0,0 +1,23 @@
+﻿/* 예) n= 5인 경우,
+**
+ **
+  **
+   **
+    **	
+*/
+
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0)
+				printf("");
+			else
+				printf(" ");
+		}
+		printf("**\n");
+	}
+}

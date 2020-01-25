0495fff2a26ea297a2b5444e6ebd773d1188f3f3
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-25 21:54:56 +0900

    1418 : t를 찾아라

---
+++ 2020 01/20200125.c
@@ -0,0 +1,12 @@
+﻿/* 어떤 문자열이 있을 때 문자 t의 위치를 모두 찾아 출력하시오. */
+
+#include <stdio.h>
+
+int main() {
+	char str[100];
+	scanf("%s", &str);
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] == 't')
+			printf("%d ", i + 1);
+	}
+}

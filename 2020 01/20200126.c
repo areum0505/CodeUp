833a3654e39a84dd56dc9057666ded8ef1b660fb
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-01-26 16:45:52 +0900

    1419 : love 2

---
+++ 2020 01/20200126.c
@@ -0,0 +1,14 @@
+﻿/* 그 문장에서 love가 몇 번 나오는지 출력하시오. */
+
+#include <stdio.h>
+
+int main() {
+	char s[200];
+	int count = 0;
+	scanf("%[^\n]", s);
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (s[i] == 'l' && s[i + 1] == 'o' && s[i + 2] == 'v' && s[i + 3] == 'e')
+			count++;
+	}
+	printf("%d \n", count);
+}

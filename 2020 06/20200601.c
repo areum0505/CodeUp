8640748adbec482855ed01f66ec6af0b907ec819
areum0505 <49233356+areum0505@users.noreply.github.com>
2020-06-01 09:48:13 +0900

    1500 : 2차원 배열에 값 저장하기

---
+++ 2020 06/20200601.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void) {
+    int arr[101][101];
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

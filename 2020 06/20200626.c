d52d9a10e92b47f9c128608974773057a1d10e58
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-06-26 09:06:28 +0900

    1733 : I.O.I

---
+++ 2020 06/20200626.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	char word[100];
+	scanf("%s", &word);
+
+	if (word[0] == 'I' && word[1] == 'O' && word[2] == 'I' && word[3] == '\0')
+		printf("IOI is the International Olympiad in Informatics.");
+	else
+		printf("I don't care.");
+
+    return 0;
+}

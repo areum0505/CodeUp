fa3126c14ef1a47e8fa17c1856e6bface2f25e17
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-07-05 22:16:15 +0900

    [기초-함수작성] 1562 ~ 1567

--- 2020 07/20200705.c
+++ 2020 07/20200705.c
@@ -92,3 +92,28 @@ int main()
   scanf("%d%d", &a, &n);
   printf("%lld\n", pow(a, n));
 }
+
+
+// 1567 : [기초-함수작성] 함수로 배열의 부분합 리턴하기
+#include <stdio.h>
+
+int n, a, b, d[1010];
+long long int subsetsum(int a, int b) {
+    long long int sum = 0;
+    for(int i = a; i <= b; i++) {
+        sum += d[i];
+    }
+    return sum;
+}
+
+int main()
+{
+  scanf("%d", &n);
+
+  for(int i=1; i<=n; i++)
+    scanf("%d", &d[i]);
+
+  scanf("%d%d", &a, &b);
+
+  printf("%lld\n", subsetsum(a, b));
+}

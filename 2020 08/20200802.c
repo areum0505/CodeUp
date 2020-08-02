a303a9fee22fea8c0d1ac5869fa041c41e183d04
Areum Kim <49233356+areum0505@users.noreply.github.com>
2020-08-02 15:05:20 +0900

    2721 : 순환 문자열

---
+++ 2020 08/20200802.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char s1[21], s2[21], s3[21];
+  scanf("%s", s1);
+  scanf("%s", s2);
+  scanf("%s", s3);
+
+  if(s3[strlen(s3)-1] == s1[0] && s1[strlen(s1)-1] == s2[0] && s2[strlen(s2)-1] == s3[0]) {
+    printf("good");
+  } else {
+    printf("bad");
+  }
+}

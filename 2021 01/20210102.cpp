0fb4e1f3a6cea07320859fbe24276253dc34a9f4
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-01-02 22:12:10 +0900

    1380 : 두 주사위의 합

---
+++ 2021 01/20210102.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int k;
+  
+	cin >> k;
+
+	for (int i = 1; i <= 6; i++) {
+		for (int j = 1; j <= 6; j++) {
+			if (i + j == k) {
+				cout << i << " " << j << endl;
+			}
+		}
+	}
+
+  return 0;
+}

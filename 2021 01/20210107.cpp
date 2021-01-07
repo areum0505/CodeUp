15e18b7a39014bd8282bb6892aa62765b39fc65d
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-01-07 22:56:49 +0900

    1405 : 숫자 로테이션

---
+++ 2021 01/20210107.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n, k[1000] = { 0 };
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> k[i];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < i + n; j++) {
+			cout <<  k[j % n] << " ";
+		}
+		printf("\n");
+	}
+}

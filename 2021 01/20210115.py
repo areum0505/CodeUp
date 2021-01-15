3af38aa3d1f1e396f80253f4175733c1fd42a931
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-01-15 20:46:56 +0900

    4058 : 암호화

---
+++ 2021 01/20210115.py
@@ -0,0 +1,12 @@
+s = input()
+s = s[::-1]
+li = []
+for i in s:
+  li.append(bin(ord(i)))
+for i in li:
+  for j in i[2:]:
+    if j == '0':
+      print(1, end = '')
+    else:
+      print(0, end = '')
+  print()

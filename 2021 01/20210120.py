53a2a7fef3215d23f69b5ba8ae7d8664844eef8c
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-01-20 21:27:06 +0900

    4832 : 세로읽기

---
+++ 2021 01/20210120.py
@@ -0,0 +1,11 @@
+arr = [[0] * 15 for _ in range(5)]
+for i in range(5):
+  a = input()
+  for j in range(len(a)):
+    arr[i][j] = a[j]
+
+for i in range(15):
+  for j in range(5):
+    if arr[j][i] != 0:
+      print(arr[j][i], end = '')
+

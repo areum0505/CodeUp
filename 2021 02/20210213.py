d1f54fb11c34aefbe34a0bb3f6f358681f96bc33
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-02-13 21:39:07 +0900

    2204 : 대칭수

---
+++ 2021 02/20210213.py
@@ -0,0 +1,11 @@
+ab = input().split()
+a = ab[0]; b = ab[1]
+max = 0
+
+for i in range(int(a), int(b) + 1):
+    for j in range(int(a), int(b) + 1):
+        if i*j == int(str(i*j)[::-1]):
+            if i*j >= max:
+                max = i*j
+
+print(max)

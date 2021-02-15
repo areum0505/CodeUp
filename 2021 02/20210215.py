00744c69b72c317ffd67c3899640794be952ee64
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-02-15 20:58:54 +0900

    2601 : 피보나치 수열

---
+++ 2021 02/20210215.py
@@ -0,0 +1,11 @@
+def f(n):
+    num = [1 for _ in range(40)]
+
+    for i in range(2, n):
+        num[i] = num[i-1] + num[i-2]
+    for i in range(0, n):
+        if i == n - 1:
+            print(num[i])
+
+n = int(input())
+f(n)

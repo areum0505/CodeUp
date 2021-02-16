c911b4a9e5be2c28c1aac22cef445c13440fb5ec
Areum Kim <49233356+areum0505@users.noreply.github.com>
2021-02-16 22:14:34 +0900

    2601 : 피보나치 수열

--- 2021 02/20210215.py
+++ 2021 02/20210215.py
@@ -1,7 +1,7 @@
 def f(n):
     num = [1 for _ in range(40)]
 
-    for i in range(2, n):
+    for i in range(2, n): 
         num[i] = num[i-1] + num[i-2]
     for i in range(0, n):
         if i == n - 1:
